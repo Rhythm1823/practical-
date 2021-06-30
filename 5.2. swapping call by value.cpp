e9f61ee841cad676367340a652f8347581d87dfc
Rhythm Pantha <83492950+Rhythm1823@users.noreply.github.com>
2021-06-30 09:30:32 +0545

    Add files via upload

---
+++ 5.2. swapping call by value.cpp
@@ -0,0 +1,23 @@
+//2. WAP TO SWAP THE VALUE OF TWO VARIABLES USING CALL BY VALUE.
+#include <stdio.h>
+void swap(int , int);
+int main()
+{
+	int a,b;
+	printf(" enter any two numbers");
+	scanf(" %d%d",&a,&b);
+	printf("before swapping: %d \t %d",a,b);
+		printf("\nafter swapping:");
+    swap(a,b);
+
+	return 0;
+}
+void swap(int a,int b)
+{
+	int c;
+	c=a;
+	a=b;
+	b=c;
+printf(" %d \t %d",a,b);
+	
+}
